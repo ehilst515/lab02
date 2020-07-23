ab3089aa346e7ad948b8c723461f95673cb82abe
Enrique Hilst <enrique.hilst@gmail.com>
2020-07-23 10:50:39 -0500

    mustache rending working

--- js/app.js
+++ js/app.js
@@ -7,7 +7,13 @@ const ajaxSettings = {
 };
 $.ajax('./data/page-1.json', ajaxSettings)
     .then((data) => {
-        console.log(data.hornedAnimal);
+        const arrayOfAnimals = data.hornedAnimal;
+        arrayOfAnimals.forEach(animal => {
+            Animal.all.push(new Animal(animal));
+        });
+    });
+$.ajax('./data/page-2.json',ajaxSettings)
+    .then((data) => {
         const arrayOfAnimals = data.hornedAnimal;
         arrayOfAnimals.forEach(animal => {
             Animal.all.push(new Animal(animal));
@@ -15,8 +21,6 @@ $.ajax('./data/page-1.json', ajaxSettings)
     })
     .then(()=>{
         renderAnimals();
-        // renderFilters();
-        // handleFilters();
     });  
 });
 
@@ -24,26 +28,8 @@ function Animal(obj){
     for(let key in obj){
         this[key] = obj[key];
     }
-    this.title = Animal.title;
-    this.image = Animal.image_url;
-    this.description = Animal.description;
-    this.keyword = Animal.keyword;
 };
 
-
-
-// // construct animal objects
-// function Animal(animal) {
-//     this.title = animal.title;
-//     this.image = animal.image_url;
-//     this.description = animal.description;
-//     this.keyword = animal.keyword;
-//     this.horns = animal.horns;
-//     this.keyword = animal.keyword;
-//     if(Animal.allKeywords.indexOf(this.keyword) < 0){
-//         Animal.allKeywords.push(this.keyword);
-//     }
-// }
 
 Animal.all = [];
 // Animal.allKeywords = [];
@@ -55,41 +41,5 @@ Animal.prototype.render = function(){
 }
 
 function renderAnimals(){
-    Animal.all.forEach(animal => $('#animals-template').append(animal.render()));
+    Animal.all.forEach(animal => $('#animal-placeholder').append(animal.render()));
 }
-
-// // render to page
-// Animal.prototype.render = function (){
-//     let $renderedAnimal = $('.animal-template').clone();
-//     $renderedAnimal.removeClass('animal-template');
-//     $renderedAnimal.find('.animal-title').text(this.title);
-//     $renderedAnimal.find('#animal-image').attr('src', this.image);
-//     $renderedAnimal.find('#animal-image').attr('alt',this.title);
-//     $renderedAnimal.find('#animal-description').text(this.description);
-//     $renderedAnimal.attr('data-keyword', this.keyword);
-//     return $renderedAnimal
-// }
-
-// function renderAnimals(){
-//     Animal.all.forEach(animal => $('#render').append(animal.render()));
-//     $('.animal-template').remove();
-// }
-
-// function renderFilters(){
-//     Animal.allKeywords.sort();
-//     Animal.allKeywords.forEach(keyword =>{
-//         const $option = $('<option>').text(keyword).attr('value', keyword);
-//             $('#keyword-filter').append($option);
-//     });
-// }
-
-// function handleFilters(){
-//     $('#keyword-filter').on('change', function() {
-//         if($(this).val() !=='default'){
-//             $('.animal').hide();
-//             $(`.animal[data-keyword="${$(this).val()}"]`).fadeIn();
-//         } else {
-//             $('.animal').fadeIn();
-//         }
-//     });
-// }
