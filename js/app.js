2f96c0fbe383c0dec00676abba37afd7f19fbdb8
Enrique Hilst <enrique.hilst@gmail.com>
2020-07-23 13:49:49 -0500

    css setup with flexbox

--- js/app.js
+++ js/app.js
@@ -21,12 +21,17 @@ $(() => {
     })
     .then(()=>{
       renderAnimals();
+      renderFilters();
+      handleFilters();
     });
 });
 
 function Animal(obj){
   for(let key in obj){
     this[key] = obj[key];
+    if(Animal.allKeywords.indexOf(this.keyword) < 0){
+      Animal.allKeywords.push(this.keyword);
+    }
   }
 }
 
@@ -40,5 +45,26 @@ Animal.prototype.render = function(){
 };
 
 function renderAnimals(){
-  Animal.all.forEach(animal => $('#animal-placeholder').append(animal.render()));
+  Animal.all.forEach(animal =>{
+    $('#animal-placeholder').append(animal.render());
+  });
+}
+
+function renderFilters(){
+  Animal.allKeywords.sort();
+  Animal.allKeywords.forEach(keyword =>{
+    const $option = $('<option>').text(keyword).attr('value', keyword);
+    $('#keyword-filter').append($option);
+  });
+}
+
+function handleFilters(){
+  $('#keyword-filter').on('change', function() {
+    if($(this).val() !=='default'){
+      $('.animal').hide();
+      $(`.animal[data-keyword="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('.animal').fadeIn();
+    }
+  });
 }
